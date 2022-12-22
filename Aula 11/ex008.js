aec03bc0e1f3471ede4d2da93dfbe1fd477ad6b9
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2022-12-22 17:07:14 -0300

    Atualizando aula JavaScript

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60) {//condição simple(só tem o if)
+    console.log('Você ultrapassou a velocidade permitida.MULTADO!')
+
+}
+console.log('Dirija sempre usando cinto de segurança')
