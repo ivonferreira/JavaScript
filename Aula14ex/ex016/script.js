8621928535241eb85f0be9aa6ffc1e6b5d80a281
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2022-12-26 15:26:52 -0300

    Atualizando Exercicios JS

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -2,13 +2,29 @@ function contar() {
     let inicio = document.getElementById('txtini')
     let fim = document.getElementById('txtfim')
     let passo = document.getElementById('txtpas')
+    let res = document.getElementById('res')
+
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
         window.alert('ERRO!Faltam dados!')
-    }else{}
-    var res = document.getElementById('res')
-    while(inicio!=fim){
-        res.innerHTML += `${inicio.value}`
-        inicio = inicio+passo
+        res.innerHTML ='Impossivel contar!'
+    }else{
+        res.innerHTML = ('Contando <br>')
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo Inválido!')
+            p = 1 // Desconsidera pass
+        }
+        if(i < f){//contagem crescente
+            for(let c = i ; c <= f ; c += p)
+            res.innerHTML +=` ${c} \u{27A1} `
+            }
+        else{//contagem decrescente
+            for(let c = i ;c >=f ; c -=p)
+            res.innerHTML += ` ${c} \u{27A1}`
+        }
+        res.innerHTML += '\u{1F3C1}'    
+    
     }
-    res.innerHTML += 'FIM!'
 }
